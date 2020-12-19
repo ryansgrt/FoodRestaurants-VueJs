1cf9458f225d9eac05351236a4797dcee23eeed8
unknown <ryansgrt23@gmail.com>
2020-12-19 14:25:50 +0700

    fetch data product & styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,26 +1,61 @@
 <template>
-  <div class="row">
-      <div class="col">
+  <div class="hero">
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md 6">
           <div class="d-flex h-100">
-              <div class="justify-content-center align-self-center">
-                  <h3><strong>Restaurant of the Future</strong><br>Just Made Ordering Food From Its Order App Much Easier</h3>
-                    <button class="btn btn-lg btn-success"><b-icon icon="cart-plus-fill" aria-hidden="true"></b-icon><cart-check-fill></cart-check-fill> Order Now </button>
-              </div>
+            <div class="justify-content-center align-self-center">
+              <h3>
+                <strong>Restaurant of the Future</strong><br />Just Made
+                Ordering Food From Its Order App Much Easier
+              </h3>
+              <button class="btn btn-lg btn-success">
+                <b-icon icon="cart-plus-fill" aria-hidden="true"></b-icon
+                ><cart-check-fill></cart-check-fill> Order Now
+              </button>
+            </div>
           </div>
-      </div>
+        </div>
 
-      <div class="col">
-          <img src="../assets/image/hero.png" width="100%">
+        <div class="col-md 6">
+          <img src="../assets/image/hero.png" width="100%" />
+        </div>
       </div>
+    </div>
+
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+          <div class="col-md 6">
+          <img src="../assets/image/hero.png" width="100%" />
+        </div>
+        <div class="col-md 6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h3>
+                <strong>Restaurant of the Future</strong><br />Just Made
+                Ordering Food From Its Order App Much Easier
+              </h3>
+              <button class="btn btn-lg btn-success">
+                <b-icon icon="cart-plus-fill" aria-hidden="true"></b-icon
+                ><cart-check-fill></cart-check-fill> Order Now
+              </button>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
+
+    
   </div>
 </template>
 
 <script>
 export default {
-    name:'Hero'
-}
+  name: "Hero",
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/main.css'
+import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -14,5 +14,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
