2a7f626021dd6b7d77aa0101cbb595403d3403ed
unknown <ryansgrt23@gmail.com>
2020-12-19 15:34:59 +0700

    create foods page

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card shadow card-product">
-    <img :src=" 'assets/images/' + product.gambar " class="card-img-top" alt="..." />
+    <img :src=" 'assets/images/' + product.gambar " class="crop" >
     <div class="card-body">
       <h5 class="card-title">{{ product.nama }}</h5>
       <p
